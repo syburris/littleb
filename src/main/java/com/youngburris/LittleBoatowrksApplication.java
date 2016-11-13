ed585c6d95b00b8343703e19b9b18b769acdd389
Steven Burris <stevenburris.jr@gmail.com>
2016-11-12 21:29:34 -0500

    Initial import;

---
+++ src/main/java/com/youngburris/LittleBoatowrksApplication.java
@@ -0,0 +1,12 @@
+package com.youngburris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleBoatowrksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleBoatowrksApplication.class, args);
+	}
+}
